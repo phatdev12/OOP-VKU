a6768c35dfa05db3374aad478a328e81b1457d59
fosly <tuphat3456tt@gmail.com>
2024-09-12 00:17:41 +0700

    feat: update main.java

--- week1/src/Main.java
+++ week1/src/Main.java
@@ -6,25 +6,21 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        Scanner obj;
+        Scanner obj = new Scanner(System.in);
 
         // #1 Hello VKU freshmen
         HelloWorld.init("Hello VKU freshmen");
 
         // #2 ASCIIToChar
-        obj = new Scanner(System.in);
         ASCIIToChar.init(obj);
 
         // #3 CharToASCII
-        obj = new Scanner(System.in);
         CharToASCII.init(obj);
 
         // #4 BasicOperators
-        obj = new Scanner(System.in);
         BasicOperators.init(obj);
 
         // #5 Circle
-        obj = new Scanner(System.in);
         Circle.init(obj);
     }
 }
